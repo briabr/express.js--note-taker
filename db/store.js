02e7255dbafed07cf606be0744d8e0c2a5b3cd2f
briabr <filalibria@yahoo.fr>
2022-10-30 18:36:54 -0400

    added the addNote() function,downgraded uuid package version

--- db/store.js
+++ db/store.js
@@ -1,32 +1,64 @@
-const util = require('util')
-const fs = require('fs')
-const readFileAsync = util.promisify(fs.readFile)
-const writeFileAsync = util.promisify(fs.writeFile)
+const util = require("util");
+const fs = require("fs");
+
+// This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
+const uuidv1 = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-    read() {
-      return readFileAsync('db/db.json', 'utf8');
-    }
-    write(){
-        //change the note to JSON so it can be sotred in the db.json file
-        return writeFileAsync ('db/db.json', JSON.stringify(note));
-    }
-    getNotes(){
-        //return the notes stored in the read() function then parse the notes and return them
-        return this.read().then((notes) => {
-            let parsedNotes;
-            // If notes isn't an array or can't be turned into one, send back a new empty array
-            try {
-              parsedNotes = [].concat(JSON.parse(notes));
-            } catch (err) {
-              parsedNotes = [];
-            }
-            return parsedNotes;
-          });
-    }
-    addNotes(){
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
 
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+
+  getNotes() {
+    //grab the title and text from the input.
+    return this.read().then((notes) => {
+      let parsedNotes;
+
+      // If notes isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
+
+  addNote(note) {
+    const { title, text } = note;
+
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
     }
+
+    // Add a unique id to the note using uuid package
+    const newNote = { title, text, id: uuidv1() };
+
+    // Get all notes, add the new note, write all the updated notes, return the newNote
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+  }
 }
 
 module.exports = new Store();
+
+
+
+
+
+
+
+
+
+
+
