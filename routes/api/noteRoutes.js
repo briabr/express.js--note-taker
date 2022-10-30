ec1cea3c915c371aa6aaa0dda4f086969104f99a
briabr <filalibria@yahoo.fr>
2022-10-30 14:28:52 -0400

    get and post for noteRoutes

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,17 +1,39 @@
 const router = require("express").Router();
-
-
+const path = require('path')
+const fs = require('fs')
+// npm package that allows for unique ids to be created
+var uniqid = require('uniqid');
 
 //GET /api/notes - gets all the notes
-router.get('/notes', (req, res) => {
-    let savedNotes = notes;
-    res.json(savedNotes);
-})
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
 
-// GET /api/notes/:id - get oen note
+router.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'));
+  });
+// GET /api/notes/:id - get one note
+
 // POST /api/notes - creates a note
+// POST /api/notes should receive a new note to save on the request body, 
+// add it to the db.json file, and then return the new note to the client. 
+router.post('/api/notes', (req, res) => {
+    let dbJson = fs.readFileSync('db/db.json');
+    dbJson = JSON.parse(dbJson);
+    res.json(dbJson);
+    // creating body for note
+    let bodyNote = {
+      title: req.body.title,
+      text: req.body.text,
+    }
+    // pushing created note to be written in the db.json file
+    dbJson.push(bodyNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(dbJson));
+    res.json(dbJson);
+
+  });
+
 //PUT /api/notes/:id - update a note
 //DELETE /api/notes/:id - delete a note 
+
 
 
 
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const routes = require("./routes")
-
+const apiRoutes = require("./routes/api")
+const htmlRoutes = require("./routes/htmlRoutes")
 
 const app = express();
+// creating environment variable port
 const PORT =  process.env.PORT || 3001;
+// asks express to create a route for every file in the 'public' folder and give it a '/' route
+app.use(express.static('public'));
 
 //middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 
 //routes
-app.use(routes)
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
